0ffe0007eea56e30e6c29f5cb3e4189b1444aafd
Bellroute <kjkun7631@gmail.com>
2023-02-20 22:11:41 +0900

    solve: 프로그래머스 - 카드 뭉치

---
+++ src/programmers/Solution155652.java
@@ -0,0 +1,36 @@
+package programmers;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Solution155652 {
+    public String solution(String[] cards1, String[] cards2, String[] goal) {
+        String answer = "";
+
+        Queue<String> cardQueue1 = new LinkedList<>();
+        for (String card : cards1) {
+            cardQueue1.offer(card);
+        }
+
+        Queue<String> cardQueue2 = new LinkedList<>();
+        for (String card : cards2) {
+            cardQueue2.offer(card);
+        }
+
+        for (String word : goal) {
+            if (!cardQueue1.isEmpty() && word.equals(cardQueue1.peek())) {
+                cardQueue1.poll();
+                continue;
+            }
+
+            if (!cardQueue2.isEmpty() && word.equals(cardQueue2.peek())) {
+                cardQueue2.poll();
+                continue;
+            }
+
+            return "No";
+        }
+
+        return "Yes";
+    }
+}
