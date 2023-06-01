c52dd9be810d7c42c4691bc7db3916e5e4859f73
Bellroute <kjkun7631@gmail.com>
2023-06-01 14:30:28 +0900

    solve: 프로그래머스 lv2 - 연속된 부분 수열의 합

---
+++ src/programmers/Solution178870.java
@@ -0,0 +1,56 @@
+package programmers;
+
+class Solution {
+    public int[] solution(int[] sequence, int k) {
+        int[] answer = new int[] {0, sequence.length};
+
+        int start = 0;
+        int end = 0;
+        int sum = sequence[0];
+
+        while (end < sequence.length - 1) {
+            if (sum == k) {
+                if (start == end) {
+                    return new int[] {start, end};
+                }
+
+                if (answer[1] - answer[0] > end - start) {
+                    answer[0] = start;
+                    answer[1] = end;
+                }
+
+                sum += sequence[++end];
+                continue;
+            }
+
+            if (sum < k) {
+                sum += sequence[++end];
+                continue;
+            }
+
+            sum -= sequence[start++];
+        }
+
+        while (start <= end) {
+            if (sum == k) {
+                if (start == end) {
+                    return new int[] {start, end};
+                }
+
+                if (answer[1] - answer[0] > end - start) {
+                    answer[0] = start;
+                    answer[1] = end;
+                }
+            }
+
+            if (sum > k) {
+                sum -= sequence[start++];
+                continue;
+            }
+
+            break;
+        }
+
+        return answer;
+    }
+}
